2d70de5ea79f87cf71c5cbc4bdff97da80f01207
giovani <gibuselli@gmail.com>
2023-03-22 22:20:16 -0300

    add: main app and page template

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class="templates")
+def pagina_inicial(request: Request):
+    dados = {
+        "pagina": "Página Inicial"
+    }
+    
+    return templates.TemplateResponse(
+        "pagina.html",
+        {"request": request, "dados": dados}
+    )
+    
+    
+@app.get("/page/{page_name}", response_class=HTMLResponse)
+def pagina_dinamica(request: Request, page_name: str):
+    dados = {
+        "pagina": nome_da_pagina
+    }
+    
+    return templates.TemplateResponse(
+        "pagina.html",
+        {"request": request, "dados": dados}
+    )
